b3edd958d867a0ba30b225cc16f380c9ce8e1210
chadd williams <chadd980@hotmail.com>
2017-04-05 17:53:06 -0400

    fixed ip address ouput

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,9 @@ router.get('/api/whoami', function(req, res) {
   // console.log(req.headers["accept-language"]);
   var language = req.headers["accept-language"].split(",")[0];
   var userAgent = /\(([^)]+)\)/.exec(req.headers["user-agent"])[1];
-  var ip = req.connection.remoteAddress;
-  // console.log(ipaddress);
+  var ip = /([^:]*)$/.exec(req.connection.remoteAddress)[0];
+  debugger
+  console.log(ip);
   res.json({ language: language, software: userAgent, ipaddress: ip});
 })
 

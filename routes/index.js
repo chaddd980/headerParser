250873440b103e6399f9ac39a6c03dc16a0cdc9c
chadd williams <chadd980@hotmail.com>
2017-04-05 17:16:32 -0400

    completed API

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/api/whoami', function(req, res) {
+  // console.log(req.headers["accept-language"]);
+  var language = req.headers["accept-language"].split(",")[0];
+  var userAgent = /\(([^)]+)\)/.exec(req.headers["user-agent"])[1];
+  var ip = req.connection.remoteAddress;
+  // console.log(ipaddress);
+  res.json({ language: language, software: userAgent, ipaddress: ip});
+})
 
 module.exports = router;
